07ffe0899690a5d965e0cc959706666e52a7535c
Ignacio Ojanguren <ignacio@privy.com>
2023-01-10 20:42:18 +0100

    Relocated css, js, and images to respective folders. Also updated App.js file

---
+++ evstreet/src/components/App.js
@@ -0,0 +1,15 @@
+import logo from "../assets/images/logo.svg";
+import "../assets/styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
