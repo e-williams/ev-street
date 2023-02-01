31998f3019acb637121d7b63bd828a430e2d6204
Evan Williams <Evan@users-MacBook-Air.local>
2023-02-01 00:46:39 -0800

    adding updated filter handling code

--- evstreet/src/components/SearchPageContainer.js
+++ evstreet/src/components/SearchPageContainer.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import "../assets/styles/SearchPageContainer.css";
 import vehicleData from "../vehicleData.json";
 import SearchContainer from "./SearchContainer";
@@ -8,23 +8,28 @@ function SearchPageContainer() {
   const [vehicleFilters, setVehicleFilters] = useState([]);
   console.log({ vehicleData });
 
-  // Function that finds all the vehicles that match the filters
-  const findVehiclesIdMatchingFilters = useMemo(() => {
-    // useMemo produces a memoized constant and the function receives a
-    // dependency array [vehicleFilters], so the produced memoized CONSTANT will
-    // only be recalculated if the value of vehicleFilters changes.
+  // Function that finds all the vehicle IDs that match the selected filters.
+  // The array of selected filters in initially empty until user input. Then,
+  // vehicleFilters is updated and this function is re-invoked, due to useMemo
+  // dependency array [vehicleFilters].
+  const findVehicleIdsMatchingFilters = useMemo(() => {
+    // useMemo produces a memoized CONSTANT value and the function receives a
+    // dependency array [vehicleFilters], so the produced CONSTANT will only be
+    // recalculated if the value of vehicleFilters changes.
     console.log("The filters have changed::: ", vehicleFilters);
-    // Return an array of elements in vehicleData that includes an element
-    // in vehicleFilters
+    // Need to return an array of data values in vehicleData that includes an
+    // element in vehicleFilters.
 
-    // Parse vehicleData: convert objects within container array to arrays
-    // of data values so can iterate over them and match values with filters
+    // Parse vehicleData: convert objects within container array to arrays.
+    // of data values so can iterate over them and match values with filters.
     const vehicleDataValues = vehicleData.map((objectData) =>
       Object.values(objectData)
+      // Produces an array of data values from objects
     );
-
     console.log("vehicleDataValues after parsing::", vehicleDataValues);
 
+    // Get only vehicle IDs matching filters so can use IDs to match original
+    // vehicle object data to pass to ResultsContainer for better output.
     const getVehicleIds = () => {
       const vehicleIdsMatchingFilters = [];
 
@@ -32,69 +37,53 @@ function SearchPageContainer() {
         vehicleFilters.forEach((filter) => {
           if (vehicle.includes(filter)) {
             vehicleIdsMatchingFilters.push(vehicle[0]);
+            // vehicle[0] selects "id" value
           }
         });
       });
-
-      console.log("filteredData is::", vehicleIdsMatchingFilters);
+      console.log("VehicleIdsMatchingFilters are::", vehicleIdsMatchingFilters);
 
       return vehicleIdsMatchingFilters;
     };
 
     return getVehicleIds();
-  }, [vehicleFilters]);
+   }, [vehicleFilters]
+  );
 
-  // Function that creates an array of all checked filters
-  const handleNewFilterSelection = useCallback(
-    (e) => {
-      // useCallback: upon subsequent renders, if the dependencies haven't
-      // changed, returns the stored FUNCTION; otherwise returns (not calls)
-      // re-rendered function.
-      // e is parameter name taking on HTML Event() interface
-      // If a checkbox is checked
-      if (e.target.checked) {
-        // if checked = true
-        setVehicleFilters([...vehicleFilters, e.target.id]);
-        // checked is <input> attribute = boolean value
-        // Event.target = target property of HTML Event interface - returns
-        // the element where the event occured.
-        // value of <inpute> attribute id is added to vehicleFilters
-        // can use spread syntax ... to add array elements
-      } else {
-        console.log("value of checked after unchecking is::", e.target.checked);
-        // ['5-door sedan', '4-door sedan']
-        console.log({ vehicleFilters });
-        let filterIndex = vehicleFilters.indexOf(e.target.id);
-        console.log("filter index is::", filterIndex);
-        console.log({ vehicleFilters }); // ['4-door sedan']
+  // Function that creates an array of all selected filters and changes the
+  // state of vehicleFilters.
+  // Function is re-invoked whenever a user clicks on a filter.
+  const handleNewFilterSelection = useCallback((e) => {
+    // useCallback: upon subsequent renders, if the dependencies haven't
+    // changed, returns the stored FUNCTION; otherwise returns (not invokes)
+    // re-rendered function.
+    // e is parameter name taking on HTML Event() interface.
+    // If a checkbox is checked:
+    if (e.target.checked) { // if checked = true
+      setVehicleFilters([...vehicleFilters, e.target.id]);
+      // checked is <input> attribute = boolean value.
+      // Event.target = target property of HTML Event interface - returns
+      // the element where the event occured.
+      // VALUE of <input> attribute id is added to vehicleFilters.
+      // Spread syntax ... to add element id value to new array state; not
+      // an array pointing to vehicleFilters memory.
+    } else {
+      console.log("value of checked after unchecking is::", e.target.checked);
+      const filterIndex = vehicleFilters.indexOf(e.target.id);
+      console.log({filterIndex});
 
-        const copyVehicleFilters = [...vehicleFilters];
+      const copyVehicleFilters = [...vehicleFilters];
+      // Spread syntax so create new array; not one pointing to memory  of
+      // vehicleFilters. Do so state update works properly, after previously
+      // being updated to value of vehicleFilters.
 
-        copyVehicleFilters.splice(filterIndex, 1);
+      copyVehicleFilters.splice(filterIndex, 1);
+      // Deletes 1 element at position filterIndex.
 
-        setVehicleFilters(copyVehicleFilters);
-      }
-    },
-    [vehicleFilters]
-  ); // [] is defined with useCallback for output
-
-  // OLD CODE:
-  // output array of data for each filter condition
-  // Get all the cars that have 4 doors
-  /*
-  const handle4DoorSedan = () =>
-    vehicleSpecsData.filter(
-      (vehicleSpec) => vehicleSpec.body_style == "4-door sedan"
-    );
-
-  // Get all the cars that have 5 doors
-  const handle5DoorSedan = () =>
-    vehicleSpecsData.filter(
-      (vehicleSpec) => vehicleSpec.body_style == "5-door sedan"
-    );
-  
-  const totalFiltersData = handle4DoorSedan().concat(handle5DoorSedan());
-  */
+      setVehicleFilters(copyVehicleFilters);
+    }
+   }, [vehicleFilters]
+  ); // [] is defined with useCallback for output.
 
   return (
     <div id="searchPageWrapper">
@@ -113,11 +102,10 @@ function SearchPageContainer() {
           <div id="resultsWrapper">
             <h2 id="resultsHeading">SEARCH RESULTS WILL GO HERE</h2>
             <p>
-              Below is some selected output simply to show JSON data & logic
-              implementation:
+              Below is some selected sample output:
             </p>
 
-            {findVehiclesIdMatchingFilters.map((vehicleId) => (
+            {findVehicleIdsMatchingFilters.map((vehicleId) => (
               <ResultsContainer
                 key={vehicleId}
                 filteredVehicleSpecs={vehicleData.find(
@@ -125,11 +113,10 @@ function SearchPageContainer() {
                 )}
               />
             ))}
-            {/* map used to produce ResultsContainer for each over JSON object
-                iteration of filteredVehicleSpecs over JSON object list.
+            {/* map used to produce ResultsContainer for each iteration of
+                vehicleId over JSON object list.
                 Each child element in a mapped list needs a unique key prop.
-                filteredVehicleSpecs = map iterator (parameter)
-                3rd filteredVehicleSpecs = property assigned to iterator */}
+                filteredVehicleSpecs is property assigned to output of find()method, which returns the 1st element in vehicleData that meets the condition. */}
           </div>
         </section>
       </div>
