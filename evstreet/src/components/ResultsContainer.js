e8dd4b257cb3f088ca01ec3e8c9f5b3d115753b3
Ignacio Ojanguren <ignacio@privy.com>
2023-02-14 15:05:49 -0500

    Implement new images

--- evstreet/src/components/ResultsContainer.js
+++ evstreet/src/components/ResultsContainer.js
@@ -1,53 +1,67 @@
-import React from 'react';
+import React from "react";
 import { Paper, Grid, ButtonBase, Typography } from "@mui/material";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 import "../assets/styles/ResultsContainer.css";
-import model_3 from "../assets/images/vehicles/tesla/Model-3-Main_23_thumbnail.png"
+import map_vehicle_to_image from "./images";
 
 function ResultsContainer({ filteredVehicleSpecs, lang }) {
   const priceToDollars = () =>
-    new Intl.NumberFormat("en-IN", {
+    new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 0,
     }).format(filteredVehicleSpecs.base_price);
-  
-  const Img = styled('img')({
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+
+  const Img = styled("img")({
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
     borderBottomLeftRadius: 5,
   });
 
+  console.log(filteredVehicleSpecs.model);
+
+  console.log("DOT:::", map_vehicle_to_image.EV6);
+
+  console.log("[]::::", map_vehicle_to_image[filteredVehicleSpecs.model]);
+
   return (
     <div id="output">
-      <Paper sx={{ 
-        backgroundColor: "#f2f2f2",
-        flexGrow: 1,
-        mb: 2 }}>
+      <Paper
+        sx={{
+          backgroundColor: "#f2f2f2",
+          flexGrow: 1,
+          mb: 2,
+        }}
+      >
         <Grid container columnSpacing={2}>
           <Grid item>
-            <Typography sx={{
-              textAlign: "center",
-              lineHeight: 1.5,
-              pt: .4,
-              backgroundColor: "rgb(175, 227, 175)",
-              borderTopLeftRadius: 5 }}>
+            <Typography
+              sx={{
+                textAlign: "center",
+                lineHeight: 1.5,
+                pt: 0.4,
+                backgroundColor: "rgb(175, 227, 175)",
+                borderTopLeftRadius: 5,
+              }}
+            >
               {filteredVehicleSpecs.make} {filteredVehicleSpecs.model}
             </Typography>
-            <ButtonBase 
-              sx={{ width: 210, height: 140 }}>
-              <Img alt="Tesla Model 3" src={model_3} />
+            <ButtonBase sx={{ width: 210, height: 140 }}>
+              <Img
+                alt="Tesla Model 3"
+                src={map_vehicle_to_image[filteredVehicleSpecs.model]}
+              />
             </ButtonBase>
           </Grid>
           <Grid item>
             <Typography
-              sx={{ mt: 2.9, fontSize: 14, color: "rgb(98, 98, 98)" }}>
+              sx={{ mt: 2.9, fontSize: 14, color: "rgb(98, 98, 98)" }}
+            >
               Base Price: {priceToDollars()}
             </Typography>
-            <Typography
-              sx={{ fontSize: 14, color: "rgb(98, 98, 98)" }}>
+            <Typography sx={{ fontSize: 14, color: "rgb(98, 98, 98)" }}>
               Body Style: {filteredVehicleSpecs.body_style}
             </Typography>
           </Grid>
---
+++ evstreet/src/components/images.js
@@ -0,0 +1,11 @@
+import model3 from "../assets/images/vehicles/tesla/Model-3-Main_23_thumbnail.png";
+import eTronGT from "../assets/images/vehicles/tesla/Model-3-Main_23_thumbnail.png";
+import ev6 from "../assets/images/vehicles/tesla/Model-3-Main_23_thumbnail.png";
+
+const map_vehicle_to_image = {
+  "Model 3": model3,
+  "e-tron GT": eTronGT,
+  EV6: ev6,
+};
+
+export default map_vehicle_to_image;
