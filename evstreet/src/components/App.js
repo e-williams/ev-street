c5a190edc3d4ec271bc730f12881f69e33bce19c
Ignacio Ojanguren <ignacio@privy.com>
2023-02-21 13:33:23 -0500

    implement router

--- evstreet/src/components/App.js
+++ evstreet/src/components/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import '../assets/styles/App.css';
-import Header from './Header';
-import SearchPageContainer from './SearchPageContainer';
-import Footer from './Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import "../assets/styles/App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import SearchPageContainer from "./SearchPageContainer";
 
 function App() {
   return (
     <div>
       <Header />
       <SearchPageContainer />
+      <Outlet />
       <Footer />
     </div>
   );
 }
 
-export default App
+export default App;
--- evstreet/src/index.js
+++ evstreet/src/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./assets/styles/index.css";
+import App from "./components/App";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <p>Oops, page not found.</p>,
+    children: [
+      { path: "/", element: <p>All the vehicles go here.</p> },
+      {
+        path: "vehicle/:vehicleId",
+        element: <p>Vehicle </p>,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element: <p>About us Page</p>,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render specified component in div element of index.html w/ id='root'
-root.render(
-  <App/>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
