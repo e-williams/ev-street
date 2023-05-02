0dad600a60f6f92a9bf5f6a5d8274ae391c47415
Evan Williams <Evan@users-MacBook-Air.local>
2023-05-02 10:13:32 -0700

    minor formatting

--- evstreet/src/config/aws.js
+++ evstreet/src/config/aws.js
@@ -4,18 +4,18 @@ aws.config.update({
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
     region: "us-west-1",
-  });
+});
 
-  const s3 = new aws.S3();
-  const bucketName = "evstreet";
+const s3 = new aws.S3();
+const bucketName = "evstreet";
 
-  const downloadImageFromS3 = async (key) => {
-    const params = {
-      Bucket: bucketName,
-      Key: key,
-    };
-    const response = await s3.getObject(params).promise();
-    return `data:${response.ContentType};base64,${response.Body.toString('base64')}`;
+const downloadImageFromS3 = async (key) => {
+  const params = {
+    Bucket: bucketName,
+    Key: key,
   };
+  const response = await s3.getObject(params).promise();
+  return `data:${response.ContentType};base64,${response.Body.toString('base64')}`;
+};
 
-  export default downloadImageFromS3;
+export default downloadImageFromS3;
