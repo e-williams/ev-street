ce6a3e4ef2cd060e25058aedbf7115d48e8e1f3c
Evan Williams <Evan@users-MacBook-Air.local>
2023-05-03 00:40:54 -0700

    minor .env change

--- evstreet/src/config/aws.js
+++ evstreet/src/config/aws.js
@@ -1,9 +1,9 @@
 import aws from "aws-sdk";
 
 aws.config.update({
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-    region: "us-west-1",
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  region: "us-west-1",
 });
 
 console.log("key", process.env.REACT_APP_AWS_ACCESS_KEY_ID);
