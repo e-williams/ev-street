29cb8b7955284fef654c089acf98f3b1cb7f8130
Evan Williams <Evan@users-MacBook-Air.local>
2023-05-09 17:06:21 -0700

    added CarouselImages loading spinner

--- evstreet/src/components/VehiclePage/CarouselImages.js
+++ evstreet/src/components/VehiclePage/CarouselImages.js
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Tooltip } from "@mui/material";
+import { Tooltip, CircularProgress, Box } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import vehicle_gallery_map from "../ImageHandling/VehicleImageMap";
 import awsDownloadImages from "../../config/aws";
+import styled from "@emotion/styled";
+
+/*
+const SpinnerBox = styled(Box)({
+  width: 380,
+  height: 670,
+  justifyContent: "center",
+});
+*/
+
+const Spinner = styled(CircularProgress)({
+  position: "relative",
+  left: "47%",
+  bottom: 220,
+})
 
 function CarouselImages({ vehicleModel }) {
 
@@ -17,7 +32,8 @@ function CarouselImages({ vehicleModel }) {
   const CarouselItem = (props) => {
 
     // Get images from aws_key in VehicleImageMap and store in state
-    const [imageData, setImageData] = useState('');
+    const [imageData, setImageData] = useState("");
+    const [imageHasLoaded, setImageHasLoaded] = useState(false);
 
     useEffect(() => {
       const fetchImage = async () => {
@@ -28,7 +44,22 @@ function CarouselImages({ vehicleModel }) {
       fetchImage();
     }, [props.item.aws_key]);
 
+    const handleImageLoad = () => {
+      setImageHasLoaded(true);
+    }
+
+    /*
+    if (imageData === "") {
+      return (
+        <SpinnerBox>
+          <CircularProgress color="success" />
+        </SpinnerBox>
+      );
+    } else {
+    */
+
     return (
+      <>
         <Tooltip
           title={`IMAGE SOURCE: ${props.item.url}`}
           arrow
@@ -36,11 +67,14 @@ function CarouselImages({ vehicleModel }) {
         >
           <img
             src={imageData}
+            onLoad={handleImageLoad}
             alt="" // must define alt to avoid compilation warning
             width="670"
             height="380"
           />
         </Tooltip>
+        { !imageHasLoaded && <Spinner color="success"/>}
+      </>              
     );
   };
 
