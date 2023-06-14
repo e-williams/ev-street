f6ec9454860f2123b944862997a5eec0b723fdaf
Evan Williams <Evan@users-MacBook-Air.local>
2023-06-13 17:19:40 -0700

    moved App.js back to Components folder

---
+++ evstreet/src/components/App.js
@@ -0,0 +1,16 @@
+import "../assets/styles/App.css";
+import Header from "./Common/Header";
+import SearchPageContainer from "./VehicleSearch/SearchPageContainer";
+import Footer from "./Common/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SearchPageContainer />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
