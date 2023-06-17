f228e246f56e019a4e472a4fef8a173c7fdabb9b
Evan Williams <Evan@users-MacBook-Air.local>
2023-06-16 23:00:48 -0700

    edited AboutUs.js

--- evstreet/src/Components/Common/Footer/AboutUs.js
+++ evstreet/src/Components/Common/Footer/AboutUs.js
@@ -31,7 +31,7 @@ function AboutUs() {
       <AboutUsWrapper maxWidth="sm">
         <HeadingTypo variant="h6">Why use EVstreet.com?</HeadingTypo>
         <StyledTypo>
-          EVstreet was born from an observed need for an efficient, powerful way to shop for and research electric vehicles. The project was founded in San Francisco, at the heart of the EV revolution, and took initial form when jQuery was practically the only JavaScript library kid on the block. We aim to provide vehicle filter capabilities that best meet the needs and preferences of the end user. We frequently re-evaluate those needs and preferences, and implement improvements to better achieve our vision. 
+          EVstreet was born from an observed need for an efficient, powerful way to shop for and research electric vehicles. The project was founded in San Francisco, near the heart of the EV revolution, and took initial form when societies' understanding and acceptance of EVs was just getting off the ground. We aim to provide vehicle filter capabilities that best meet the needs and preferences of the end user, and to include only the most sensible information, while excluding supurfluous clutter. We frequently re-evaluate our strategy and implement improvements to better achieve our vision.
         </StyledTypo>
         <StyledTypo>
           We at EVstreet are passionate about sustainability and the health of our natural environment. We strive to achieve a product that reflects that passion, and hope to aid in the facilitatation of a worldwide transition to vehicles powered by alternatives to combustion engines.
